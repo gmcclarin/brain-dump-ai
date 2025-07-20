62385c7ac68e6966b7451d2ed2d0a9f9b0a40833
Grace Nieboer <grace.nieboer@outlook.com>
2025-07-20 19:50:17 -0400

    Form component

---
+++ client/src/components/Form.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@mui/material";
+import type { ReactNode } from "react";
+
+type FormProps = {
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    children: ReactNode;
+    submitLabel?: string;
+}
+export default function Form({onSubmit, children, submitLabel}: FormProps) {
+    return(
+        <form onSubmit={onSubmit} className="flex flex-col gap-4">
+            {children}
+            <Button
+            type="submit"
+            className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
+
+            >
+                {submitLabel}
+            </Button>
+        </form>
+    )
+}
