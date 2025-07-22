71fbf0fc24a7cffae1e45c7c10c2d65e24ec09dc
Grace Nieboer <grace.nieboer@outlook.com>
2025-07-21 20:30:35 -0400

    create note hook

--- client/src/api/notes.ts
+++ client/src/api/notes.ts
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {type NoteFormValues } from "../components/NoteForm";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const PATH = "notes";
 
+// GET 
 const getNotes = async() => {
     const response = await fetch(`${BASE_URL}/${PATH}`);
 
@@ -15,5 +17,27 @@ export const useNotes = () => {
     return useQuery({
         queryKey: ["notes"],
         queryFn: getNotes
+    })
+}
+
+//POST
+const createNote = async (note: NoteFormValues) => {
+    const response = await fetch(`${BASE_URL}/${PATH}`, {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(note)
+    });
+
+    if (!response.ok) throw new Error ("Failed to create note");
+    return response.json();
+}
+
+export const useCreateNote = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: createNote,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["notes"]})
+        }
     })
 }
